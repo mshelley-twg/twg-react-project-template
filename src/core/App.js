62f0955a61f7c10bbea40cb43912eba60bd8ea8f
Matt Shelley <mshelley@theworkinggroup.ca>
2019-11-29 14:09:28 -0500

    Create HelloWorld route at /

--- src/core/App.js
+++ src/core/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import styles from './App.module.scss'
+import { Routes } from './Routes'
 
 export const App = () => (
-  <main className={styles.app}>
-    Hello World
-  </main>
+  <Router>
+    <main className={styles.app}>
+      <Routes />
+    </main>
+  </Router>
 )
