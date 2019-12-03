ce8e5feba486a126e14b60367739ebc4ade4d8aa
Matt Shelley <mshelley@theworkinggroup.ca>
2019-12-03 14:37:38 -0500

    Create example accordion component with classnames for styles

--- src/core/HelloWorld.js
+++ src/core/HelloWorld.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
+import { Accordion } from 'common/components'
+
 export const HelloWorld = () => (
-  <p>
-    Hello World
-  </p>
+  <>
+    <p>
+      Hello World
+    </p>
+    <Accordion title='Accordion'>
+      Click the accordion header to hide the accordion content
+    </Accordion>
+  </>
 )
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import 'common/styles/index.scss'
+
 import { App } from 'core/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
